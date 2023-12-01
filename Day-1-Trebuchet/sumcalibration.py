43636d967f0d1ff87fee0bed103da438fecf1623
John <robinsapan@outlook.com>
2023-12-01 16:29:04 -0500

    Complete day 1 puzzles

--- Day-1-Trebuchet/sumcalibration.py
+++ Day-1-Trebuchet/sumcalibration.py
@@ -1,13 +1,46 @@
-import re
+data = open("Day-1-Trebuchet/data.txt")
+sum = 0
+digits = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+]
 
-data = open("data.txt")
-sum=0
+
+def indices(n, w, start=0):
+    index = n.find(w, start)
+    if index == -1:
+        return []
+    return [index] + indices(n, w, index + 1)
+
+
+def append_indices(foundnum, indices, word, number):
+    if indices:
+        # print(indices, word, number + 1)
+        for j in indices:
+            foundnum.update({j: number + 1})
+
+
 for x in data:
-    #print(x)
-    y = re.findall('[0-9]', x)
-    print(y[0] + y[-1])
-    sum += int(y[0] + y[-1])
-    print(sum)
+    # print(x)
+    foundnum = {}
+    for idx, word in enumerate(digits):
+        i = indices(x, word)
+        n = indices(x, str(idx + 1))
+        append_indices(foundnum, i, word, idx)
+        append_indices(foundnum, n, word, idx)
+    ha = list(foundnum.keys())
+    ha.sort()
+    num = int("{}{}".format(foundnum.get(ha[0]), foundnum.get(ha[-1])))
+    # print(num)
+    sum += num
+print(sum)
 
 
 data.close()
